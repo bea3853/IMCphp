ca42f4c3fcce415704928ec7b6783637d2e4de2d
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2022-01-30 10:20:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+
+
+<div>
+<p> O seu IMC é: </p>
+<?php
+
+$altura = 1.80;
+$peso = 80;
+$imc= $peso / ($altura * $altura);
+
+$faixa = "";
+
+if (*imc < 18.5 && $imc <25){
+$faixa = "Magreza";
+}else if ($imc >= 18.5 && $imc <25){
+$faixa = "Normal";
+}else if ($imc >= 25 && $imc <30){
+$faixa  = "Sobrepeso";
+}else {
+$faixa  = "Obesidade";
+}
+
+echo $faixa; 
+
+
+?>
+</b>
+</p>
+</div>
